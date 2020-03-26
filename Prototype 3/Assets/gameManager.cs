61c8710fef0351966c021f1838de50454ab93f63
Harout77 <harout@live.ca>
2020-03-26 15:38:46 -0400

    Prototype 3 update 2

--- Prototype 3/Assets/gameManager.cs
+++ Prototype 3/Assets/gameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class gameManager : MonoBehaviour
 {
@@ -9,6 +11,24 @@ public class gameManager : MonoBehaviour
     bool EndGame = false;
     public float restartDelay = 2f;
 
+
+
+
+
+
+
+
+
+
+    public Text canvasText1;
+    void Start()
+    {
+        Invoke("DisableText", 3f);//invoke after 3 seconds
+    }
+    void DisableText()
+    {
+        canvasText1.enabled = false;
+    }
 
     public GameObject levelCompleteUI;
 
