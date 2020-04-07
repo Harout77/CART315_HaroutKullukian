eb9d42f71da2f5eda1a1027df3933c45a27f43c5
Harout77 <harout@live.ca>
2020-04-07 14:11:02 -0400

    character select

---
+++ Prototype 3/Assets/select.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+
+public class select : MonoBehaviour
+{
+
+
+
+
+
+
+
+
+
+
+    public void ButtonClick()
+    {
+
+
+        if (EventSystem.current.currentSelectedGameObject.name == "sphere") // CODE FOR "SELECT" BUTTON
+
+        {
+            SceneManager.LoadScene("Tutorial_sphere");
+
+        }
+
+
+
+
+
+        if (EventSystem.current.currentSelectedGameObject.name == "cube") // CODE FOR "SELECT" BUTTON
+
+        {
+            SceneManager.LoadScene("Tutorial_cube");
+
+        }
+
+
+    }
+
+
+}
